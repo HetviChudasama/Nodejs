d7f729e414ec491a0beced3f6f6d603d354eee99
HetviChudasama <hetvi3034.hc@gmail.com>
2025-06-23 22:21:04 +0530

    core_modeules_overview

---
+++ core_modules.js
@@ -0,0 +1,19 @@
+// what are the core modules ?
+// what are global modules?
+// gobal model example
+// non-global modules with example
+// Interview Question
+
+// 1. Core modules :
+
+// fs -> file system ,Buffer,HTTP
+
+// 2. Global Modules :
+
+const fs = require('fs');
+fs.writeFileSync("Hello.txt" , "learning node js " );
+
+console.log(__dirname);
+
+console.log("->>" , __filename);
+
