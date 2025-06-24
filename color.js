a44e75421182804bd99b09dab3151089fe78c55c
HetviChudasama <hetvi3034.hc@gmail.com>
2025-06-24 21:50:43 +0530

    chalk pakage

--- color.js
+++ color.js
@@ -1,4 +1,19 @@
-const colors = require('colors');
+import chalk from 'chalk'
+// const colors = require('colors');
 
-console.log('hello'.red);
-console.log("My name is hetvi".green);
+// console.log('hello'.red);
+// console.log("My name is hetvi".green);
+
+
+
+console.log(chalk.blue('hello world!'));
+
+// Combine styled and normal strings
+console.log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+console.log(chalk.blue('hello world!'));
+console.log(chalk.green("My name is Hetvi"));
+console.log(chalk.red("This is red text"));
+console.log(chalk.yellow("Warning message"));
+console.log(chalk.magenta("Fun with colors!"));
+console.log(chalk.blue('Hello') + ' World ' + chalk.red('!'));
